fca9f735d0b5d8227a18eeba2f44409c1d291eb5
K2155 <K2155@OHJE-330-WS-22>
2017-01-19 11:00:59 +0200

    Uusin

--- Teht12/Program.cs
+++ Teht12/Program.cs
@@ -10,7 +10,24 @@ namespace Teht12
     {
         static void Main(string[] args)
         {
-            
+            int[] taulukko = new int[5];
+
+
+            Console.Write("Anna luku ");
+            taulukko[0] = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku ");
+            taulukko[1] = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku ");
+            taulukko[2] = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku ");
+            taulukko[3] = int.Parse(Console.ReadLine());
+            Console.Write("Anna luku ");
+            taulukko[4] = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(taulukko[4] + " " + taulukko[3] + " " + taulukko[2] + " " + taulukko[1] + " " + taulukko[0]);
+
+
         }
     }
 }
