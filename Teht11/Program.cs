3bc18fcd12158e170238f9e84b71660a5db856df
K2155 <K2155@OHJE-330-WS-22>
2017-01-19 10:46:08 +0200

    Uudet

--- Teht11/Program.cs
+++ Teht11/Program.cs
@@ -13,17 +13,18 @@ namespace Teht11
             int luku;
             Console.Write("Anna luku ");
             luku = int.Parse(Console.ReadLine());
+            
 
             for (int rivi = 1; rivi <= luku; rivi++)
             {
-                for (int pysty = 1; pysty <= rivi; pysty++)
+                for (int sivu = 1; sivu <= rivi; sivu++)
                 {
-                    Console.WriteLine("*");
+                    Console.Write("*");
                 }
-                Console.Write("\n");
 
+                Console.WriteLine();
             }
-
+            
 
         }
     }
