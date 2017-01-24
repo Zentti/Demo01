11c9ae5bc96b5432c6158039ed77f278cb7b27ea
Zentti <nikopoutanen@outlook.com>
2017-01-24 20:57:06 +0200

    Uusia

--- Teht13/Program.cs
+++ Teht13/Program.cs
@@ -10,7 +10,34 @@ namespace Teht13
     {
         static void Main(string[] args)
         {
+            int[] taulukko = new int[5];
+            int temp;
+            int summa = 0;
+            Console.WriteLine("Anna pisteet ");
+            taulukko[0] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna pisteet ");
+            taulukko[1] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna pisteet ");
+            taulukko[2] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna pisteet ");
+            taulukko[3] = int.Parse(Console.ReadLine());
+            Console.WriteLine("Anna pisteet ");
+            taulukko[4] = int.Parse(Console.ReadLine());
 
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    if (taulukko[j] > taulukko[j + 1])
+                    {
+                        temp = taulukko[j];
+                        taulukko[j] = taulukko[j + 1];
+                        taulukko[j + 1] = temp;
+                    }
+                } 
+            }
+            summa = (taulukko[1] + taulukko[2] + taulukko[3]);
+            Console.WriteLine("Kokonaispisteet ovat " + summa); 
         }
     }
 }
---
+++ Teht14/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int luku0 = 0;
+            int luku1 = 0;
+            int luku2 = 0;
+            int luku3 = 0;
+            int luku4 = 0;
+            int luku5 = 0;
+            int kirjotettu;
+            int laskuri = 0;
+
+            while (laskuri < 8)
+            {
+                Console.Write("Anna arvosana asteikolla 0-5 ");
+                kirjotettu = int.Parse(Console.ReadLine());
+                laskuri++;
+
+                switch (kirjotettu)
+                {
+                    case 0:
+                        luku0++;
+                        break;
+
+                    case 1:
+                        luku1++;
+                        break;
+
+                    case 2:
+                        luku2++;
+                        break;
+
+                    case 3:
+                        luku3++;
+                        break;
+
+                    case 4:
+                        luku4++;
+                        break;
+
+                    case 5:
+                        luku5++;
+                        break;
+
+                    default:
+                        Console.WriteLine("Arvosana ei ollut asteikolta 0-5! ");
+                        break;
+                }
+            }
+            Console.WriteLine("Arvosanat:");
+            Console.WriteLine("0: ");
+            for (int i = 0;i<luku0;i++)
+                Console.Write("*");
+
+            Console.WriteLine();
+            Console.WriteLine("1: ");
+            for (int i = 0; i < luku1; i++)
+                Console.Write("*");
+
+            Console.WriteLine();
+            Console.WriteLine("2: ");
+            for (int i = 0; i < luku2; i++)
+                Console.Write("*");
+
+            Console.WriteLine();
+            Console.WriteLine("3: ");
+            for (int i = 0; i < luku3; i++)
+                Console.Write("*");
+
+            Console.WriteLine();
+            Console.WriteLine("4: ");
+            for (int i = 0; i < luku4; i++)
+                Console.Write("*");
+
+            Console.WriteLine();
+            Console.WriteLine("5: ");
+            for (int i = 0; i < luku5; i++)
+                Console.Write("*");
+
+            Console.WriteLine();
+        }
+    }
+}
