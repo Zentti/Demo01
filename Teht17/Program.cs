06327f17f18ee943e82aba0d6c439d463bc25b42
K2155 <K2155@TIET-327-WS-37>
2017-01-25 15:04:25 +0200

    Uusimmat

---
+++ Teht17/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] taulukko1 = { 10, 20, 30, 40, 50 };
+            int[] taulukko2 = { 5, 15, 25, 35, 45 };
+            int[] yhdtaul = new int[10];
+            int j = 5;
+            int temp;
+
+            for (int i = 0; i<5;i++,j++)
+            {
+                yhdtaul[i] = taulukko1[i];
+                yhdtaul[j] = taulukko2[i];
+            }
+           
+            for (int i = 0; i <= 9; i++)
+            {
+                for (int k = 0; k < 9; k++)
+                {
+                    if (yhdtaul[k] > yhdtaul[k + 1])
+                    {
+                        temp = yhdtaul[k];
+                        yhdtaul[k] = yhdtaul[k + 1];
+                        yhdtaul[k + 1] = temp;
+                    }
+                }
+            }
+            foreach (int luku in yhdtaul)
+            {
+                Console.Write(luku + ", ");
+            }
+        }
+    }
+}
---
+++ Teht18/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sana;
+            bool testi = true;
+
+            Console.Write("Anna sana ");
+            sana = Console.ReadLine();
+
+            for (int i = 0; i < sana.Length/2; i++)
+            {
+                if(sana[i] != sana[sana.Length -1 - i])
+                {
+                    Console.WriteLine("Sana ei ole palindromi!");
+                    testi = false;
+                    break;
+                } 
+    
+            }
+            if (testi == true)
+            {
+                Console.WriteLine("Sana on palindromi!");
+            }
+        }
+    }
+}
